7d4b17f48646396179c9b930bc826ce23242e712
Mihael <mihael.pavlakovic@gmail.com>
2025-03-27 17:57:06 +0100

    feat: add gh-pages to project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/svijet-apartmana-app/'
-    : '/'
-}
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/svijet-apartmana/" : "/",
+};
